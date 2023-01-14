e86b3e1c195d5454afbfbdcaba5e53233da28a6a
9names <60134748+9names@users.noreply.github.com>
2023-01-14 12:37:49 +1100

    Move resetValue from field into Register to match SVD spec

--- bl808-data/src/parser.rs
+++ bl808-data/src/parser.rs
@@ -26,10 +26,22 @@ impl Register {
 impl fmt::Display for Register {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "<register>\n<name>{}</name>\n<description>{}</description>\n<address_offset>{}</address_offset>\n<fields>\n", self.name, self.description, self.address_offset)?;
+        // Assuming u32 register sizes for now
+        let mut reset_value: u32 = 0;
         for field in &self.fields {
             write!(f, "{}", field)?;
+            if let Some(reset_value_str) = field.reset_value.strip_prefix("0x") {
+                let value = u32::from_str_radix(reset_value_str, 16).unwrap();
+                let offset = field.lsb.parse::<u32>().unwrap();
+                reset_value += value << offset;
+            }
         }
-        write!(f, "</fields>\n</register>")
+        write!(
+            f,
+            // 8 digits + "0x" prefix == 10 characters
+            "</fields>\n<resetValue>{:#010X}</resetValue>\n</register>",
+            reset_value
+        )
     }
 }
 
@@ -71,8 +83,8 @@ impl fmt::Display for Field {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            "<field>\n<name>{}</name>\n<description>{}</description>\n<bitRange>[{}:{}]</bitRange>\n<access>{}</access>\n<resetValue>{}</resetValue>\n</field>\n",
-            self.name, self.description, self.msb, self.lsb, svd_access_map(&self.access), self.reset_value,
+            "<field>\n<name>{}</name>\n<description>{}</description>\n<bitRange>[{}:{}]</bitRange>\n<access>{}</access>\n</field>\n",
+            self.name, self.description, self.msb, self.lsb, svd_access_map(&self.access),
         )
     }
 }
