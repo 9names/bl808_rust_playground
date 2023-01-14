5b52289b0822a552cc630cdce48b8399a09273c3
9names <60134748+9names@users.noreply.github.com>
2023-01-14 22:14:19 +1100

    Add the rest of the header files to svd_generator

--- bl808-data/src/bin/svd_generator.rs
+++ bl808-data/src/bin/svd_generator.rs
@@ -13,24 +13,30 @@ macro_rules! regex {
     }};
 }
 
+// Convenience wrapper for calling with a single filename
 fn peripheral(filename: &str, fragment: &str) -> Result<(), std::io::Error> {
-    let f = std::fs::read(filename)?;
+    peripherals(&[filename], fragment)
+}
 
+fn peripherals(filenames: &[&str], fragment: &str) -> Result<(), std::io::Error> {
     // Create our parse context
     let mut parser = Parser::new();
 
-    println!("<peripheral>\n{}", fragment);
-    // Run all the input through the parser
-    for (linenum, l) in f.split(|b| b == &b'\n').enumerate() {
-        let l = String::from_utf8_lossy(l);
-        parser.parse(linenum, l.to_string());
+    println!("<peripheral>\n{}<registers>\n", fragment);
+
+    for filename in filenames {
+        let f = std::fs::read(filename)?;
+        for (linenum, l) in f.split(|b| b == &b'\n').enumerate() {
+            let l = String::from_utf8_lossy(l);
+            parser.parse(linenum, l.to_string());
+        }
     }
 
     // Dump out all the registers
     for register in parser.registers() {
         print!("{}", register);
     }
-    print!("\n</peripheral>");
+    print!("\n</registers>\n</peripheral>");
     Ok(())
 }
 
@@ -40,8 +46,50 @@ fn main() -> anyhow::Result<()> {
         .with_max_level(Level::INFO) // Set this to DEBUG or TRACE to get debugging info
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
-    println!("{}", svd_fragments::HEADER);
+    print!("{}", svd_fragments::HEADER);
+    let _ = peripheral(
+        "sources/headers/bl_mcu_sdk/codec_misc_reg.h",
+        svd_fragments::CODEC,
+    );
+    let _ = peripheral("sources/headers/bl_mcu_sdk/aon_reg.h", svd_fragments::AON);
+    let _ = peripheral("sources/headers/bl_mcu_sdk/cci_reg.h", svd_fragments::CCI);
+    // Display controller?
+    // let _ = peripheral("sources/headers/bl_mcu_sdk/dtsrc_reg.h", svd_fragments::);
+    let _ = peripherals(
+        &[
+            "sources/headers/bl_mcu_sdk/ef_ctrl_reg.h",
+            "sources/headers/bl_mcu_sdk/ef_data_0_reg.h",
+            "sources/headers/bl_mcu_sdk/ef_data_1_reg.h",
+        ],
+        svd_fragments::EF_DATA,
+    );
     let _ = peripheral("sources/headers/bl_mcu_sdk/glb_reg.h", svd_fragments::GLB);
+    let _ = peripheral("sources/headers/bl_mcu_sdk/gpip_reg.h", svd_fragments::GPIP);
+    let _ = peripheral("sources/headers/bl_mcu_sdk/hbn_reg.h", svd_fragments::HBN);
+    let _ = peripheral("sources/headers/bl_mcu_sdk/ipc_reg.h", svd_fragments::IPC0);
+    let _ = peripheral(
+        "sources/headers/bl_mcu_sdk/mcu_misc_reg.h",
+        svd_fragments::MCU_MISC,
+    );
+    // These might be for c906
+    // let _ = peripheral("sources/headers/bl_mcu_sdk/mm_glb_reg.h", svd_fragments::GLB);
+    // let _ = peripheral("sources/headers/bl_mcu_sdk/mm_misc_reg.h", svd_fragments::);
+    let _ = peripheral("sources/headers/bl_mcu_sdk/pds_reg.h", svd_fragments::PDS);
+    let _ = peripherals(
+        &[
+            "sources/headers/bl_mcu_sdk/psram_reg.h",
+            "sources/headers/bl_mcu_sdk/psram_uhs_reg.h",
+        ],
+        svd_fragments::PSRAM_CTRL,
+    );
+    let _ = peripheral("sources/headers/bl_mcu_sdk/sdh_reg.h", svd_fragments::SDH);
+
+    // let _ = peripheral("sources/headers/bl_mcu_sdk/sf_ctrl_reg.h", svd_fragments::SF_CTRL);
+
+    // let _ = peripheral("sources/headers/bl_mcu_sdk/tzc_nsec_reg.h", svd_fragments::TZC_NSEC);
+    // let _ = peripheral("sources/headers/bl_mcu_sdk/tzc_sec_reg.h", svd_fragments::TZC_SEC);
+    // let _ = peripheral("sources/headers/bl_mcu_sdk/.h", svd_fragments::);
+
     println!("{}", svd_fragments::FOOTER);
 
     Ok(())
