aebd7c751be6d9dc7d3b0bbf7fbaea085ad9c9ff
9names <60134748+9names@users.noreply.github.com>
2023-01-22 19:31:29 +1100

    Set up 3 GPIO in multiboot, one for each core

--- bl808-multiboot/readme.md
+++ bl808-multiboot/readme.md
@@ -1,3 +1,7 @@
 # BL808 multiboot
 
 Attempts to get D0 and LP cores up and going
+
+```system
+cargo objcopy --release -- -O binary firmware.bin && bflb-iot-tool --chipname bl808 --firmware firmware.bin --baudrate 2000000 --port /dev/ttyACM1
+```
--- bl808-multiboot/src/main.rs
+++ bl808-multiboot/src/main.rs
@@ -9,16 +9,25 @@ use embedded_hal::blocking::delay::DelayMs;
 use embedded_hal::blocking::delay::DelayUs;
 use riscv::delay::McycleDelay;
 
+// include the D0 firmware in this binary. doesn't work yet. TODO.
+
+// const D0_FIRMWARE_SIZE:usize = include_bytes!("../../bl808-d0-blinky/d0_blinky.bin").len();
+// #[link_section = ".d0"]
+// #[used]
+// pub static D0_FIRMWARE: [u8; D0_FIRMWARE_SIZE] = *include_bytes!("../../bl808-d0-blinky/d0_blinky.bin");
+
 #[riscv_rt::entry]
 fn main() -> ! {
     let mut delay = McycleDelay::new(SYSFREQ);
     let peris = unsafe { pac::Peripherals::steal() };
 
-    // Set up GPIO18 as an output LED now
+    // Set up GPIOs as outputs now
     // this will make it easier to test LP/D0 firmware
-    // without pulling in PAC
-    let led = &peris.GLB.gpio_cfg18;
-    led.modify(|_, w| {
+    // GPIO18 is used for LP core
+    // GPIO17 is used for M0 core
+    // GPIO16 is used for D0 core
+    let led_lp = &peris.GLB.gpio_cfg18;
+    led_lp.modify(|_, w| {
         unsafe {
             w.reg_gpio_18_func_sel().bits(11);
         }
@@ -29,15 +38,35 @@ fn main() -> ! {
         w
     });
 
-    // Set LED high now
+    // Set up GPIO 17 for m0
+    let led = &peris.GLB.gpio_cfg17;
     led.modify(|_, w| {
-        w.reg_gpio_18_o().clear_bit();
+        unsafe {
+            w.reg_gpio_17_func_sel().bits(11);
+        }
+        w.reg_gpio_17_ie().clear_bit();
+        w.reg_gpio_17_oe().set_bit();
+        w.reg_gpio_17_pd().clear_bit();
+        w.reg_gpio_17_pu().clear_bit();
         w
     });
 
-    peris.GLB.swrst_cfg2.modify(|_,w| {
-        w.reg_ctrl_cpu2_reset().clear_bit();
-        w.reg_ctrl_pico_reset().clear_bit();
+    // And GPIO 16 for d0
+    let led3 = &peris.GLB.gpio_cfg16;
+    led3.modify(|_, w| {
+        unsafe {
+            w.reg_gpio_16_func_sel().bits(11);
+        }
+        w.reg_gpio_16_ie().clear_bit();
+        w.reg_gpio_16_oe().set_bit();
+        w.reg_gpio_16_pd().clear_bit();
+        w.reg_gpio_16_pu().clear_bit();
+        w
+    });
+
+    // Set LED high now
+    led.modify(|_, w| {
+        w.reg_gpio_17_o().clear_bit();
         w
     });
 
@@ -51,7 +80,7 @@ fn main() -> ! {
     // wait a jiffy
     delay.delay_us(1);    
 
-    // Update LP code start address
+    // // Update LP code start address
     peris.PDS_BASE.cpu_core_cfg13.modify(|_,w| {
         unsafe{
             w.e902_rst_addr().bits(0x58002000);
@@ -78,6 +107,17 @@ fn main() -> ! {
     });
 
     loop {
-        delay.delay_ms(200);
+        // Set LED high
+        led.modify(|_, w| {
+            w.reg_gpio_17_o().clear_bit();
+            w
+        });
+        delay.delay_ms(2000);
+        // // // Set LED low
+        led.modify(|_, w| {
+            w.reg_gpio_17_o().set_bit();
+            w
+        });
+        delay.delay_ms(2000);
     }
 }
